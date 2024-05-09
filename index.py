98ec44dbe05e4dcf7d358148e51d6e97bc95be54
Rasyidannas <rasshit.dsgn@gmail.com>
2024-05-10 05:45:27 +0700

    ChatModels: GPT-3.5 Turbo and GPT-4

--- index.py
+++ index.py
@@ -1,5 +1,29 @@
+# Python-dotenv
 import os
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv(), override=True)
 
-os.environ.get('PINECONE_API_KEY')
+# os.environ.get('OPENAI_API_KEY')
+
+# Chat Models: GPT-3.5 Turbo and GPT-4
+from langchain_openai import ChatOpenAI
+llm = ChatOpenAI()
+output = llm.invoke('Explain quantum mechanics in one sentence.', model='gpt-3.5-turbo')
+print(output.content)
+
+# this is for show detail our configuration CHatOpenAI
+# help(ChatOpenAI)
+
+from langchain.schema import (
+    SystemMessage,
+    AIMessage,
+    HumanMessage
+)
+
+messages = [
+    SystemMessage(content='You are a physicist and respond only in Indonesian'),
+    HumanMessage(content='Explain quantum mechanics in one sentence.')
+]
+
+output = llm.invoke(messages)
+print(output.content)
